95e7333d09c954eb86da58c1ce98b7ef65ae0ed2
Adesina Opeoluwa Martins <opehardesina@gmail.com>
2021-06-15 21:43:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState,useRef,useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Square from './components/Square/Square'
 
 function App() {
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
+  const [horizontalAxis,setHorizontalAxis] = useState([]);
+  const [verticalAxis,setVerticalAxis] = useState([]);
+ 
+   
+  const gameBoard = useRef();
+  // const gameBoard = useRef();
+  // if (horizontalAxis.length>0 &verticalAxis.length>0) {
+  //   console.log(gameBoard.current.childNodes[0].childNodes[0].style.backgroundColor='red')
+  // }
+
+  useEffect(()=>{
+       console.log('hey1')
+  if (horizontalAxis.length>0 &verticalAxis.length>0) {
+    console.log(gameBoard.current.childNodes[0].childNodes[0].style.backgroundColor='red')
+   }
+  },[horizontalAxis])
+ 
+  const startGame=()=>{
+    console.log(width,height)
+    let amountOfSquares = Array.from({length: width}, (v, i) => i);
+    let amountOfSquareRows = Array.from({length: height}, (v, i) => i);
+    setHorizontalAxis([...amountOfSquares])
+    setVerticalAxis([...amountOfSquareRows])
+    console.log(horizontalAxis)
+
+   
+    
+  }
+  let squares
+  // current.childNodes[0].childNodes[0]
+  if (horizontalAxis.length > 0) {
+      squares = (
+        <div style={{display:'flex'}}>
+         {horizontalAxis.map(()=><Square />)}
+       </div>
+      )
+   }
+  
+  let generateRandomSpritePositions=(coordinateX,coordinateY)=>{
+     
+  } 
+
+ let setStartingPosition=()=>{
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", justifyContent: "center",marginTop:'10px'}}>
+        <div style={{margin:'0 10px 0 10px'}}>
+          <TextField required id="standard-required" label="width" onChange={event=>setWidth(event.target.value)} />
+        </div>
+        <div style={{margin:'0 10px 0 10px'}}>
+          <TextField required id="standard" label="height" onChange={event=>setHeight(event.target.value)} />
+        </div>
+        <Button variant="contained" color="primary" onClick={startGame}>
+          Start Game
+        </Button>
+      </div>
+      
+      <div style={{display:'flex',justifyContent:'center'}}>
+         <div style={{marginTop:'50px'}} ref={gameBoard}>
+          {verticalAxis.map(()=>squares)}
+         </div>
+      </div>
+       
     </div>
   );
 }
---
+++ src/components/Square/Square.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Square =()=>{
+
+    return(
+     <div style={{width:'25px',height:'25px',border:'1px solid black'}}>
+
+     </div>
+    )
+}
+
+
+export default Square
